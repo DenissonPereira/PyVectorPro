d7dbae134f19086d3056fdd7cf4658c77d7848d8
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-06 21:34:49 -0300

    Update PyVectorPro.py

--- PyVectorPro.py
+++ PyVectorPro.py
@@ -1,37 +1,21 @@
 from math import cos, sin, radians, atan, degrees
 
-massa = float(input("Qual o valor da massa? (Digite 0 para calcular apenas a força)"))
+massa = float(input("Digite o valor da massa: "))
+qnt_F = int(input("Qual a quantidade de forças no sistema? "))
 
-Forca1 = float(input("Qual o módulo da primeira força? "))
-angulo1 = float(input("Qual o primeiro ângulo? "))
+contador = 1
+F_x_resultante = 0
+F_y_resultante = 0
 
-Fx1 = Forca1 * cos(radians(angulo1))
-Fy1 = Forca1 * sin(radians(angulo1))
+while contador <= qnt_F:
+    F = float(input("Qual o módulo de F{}? ".format(contador)))
+    teta = float(input("Qual o teta{}? ".format(contador)))
 
-Forca2 = float(input("Qual o módulo da segunda força? "))
-angulo2 = float(input("Qual o segundo ângulo? "))
+    Fx = F * cos(radians(teta))
+    Fy = F * sin(radians(teta))
 
-Fx2 = Forca2 * cos(radians(angulo2))
-Fy2 = Forca2 * sin(radians(angulo2))
+    F_x_resultante += Fx
+    F_y_resultante += Fy
+    contador += 1
 
-
-F_resultante_x = Fx1 + Fx2
-F_resultante_y = Fy1 + Fy2
-
-if massa == 0:
-    print("Fx = {} Fy = {}.".format(F_resultante_x, F_resultante_y))
-else:
-    aceleracao_x = (F_resultante_x / massa)
-    aceleracao_y = (F_resultante_y / massa)
-
-    aceleracao = (((aceleracao_x)**2) + ((aceleracao_y)**2))**(1/2)
-
-    div = (aceleracao_y / aceleracao_x)
-
-    arctan = atan(div)
-    arctan_graus = degrees(arctan)
-    print("Fx = {} Fy = {}.".format(F_resultante_x, F_resultante_y))
-    print("a_x = {} e a_y = {}.".format(aceleracao_x, aceleracao_y))
-    print("O módulo da aceleração é {}.".format(aceleracao))
-    print("O ângulo da aceleração é {}.".format(arctan_graus))
-
+print("Fx = {} e Fy = {}.".format(F_x_resultante, F_y_resultante))
